9143b35587f639c010f8e612330a7005dc0857bd
positron <769230030@qq.com>
2026-06-10 23:45:01 +0800

    add special config for .hs

--- ftplugin/haskell.lua
+++ ftplugin/haskell.lua
@@ -1,1 +1,5 @@
 vim.lsp.enable("hls")
+
+vim.opt.tabstop = 2                             -- Tab 键宽度为 2
+vim.opt.softtabstop = 2                         -- Tab 键宽度为 2
+vim.opt.shiftwidth = 2                          -- 缩进宽度为 2
