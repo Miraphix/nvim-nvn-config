815f59842429e4873343803b3c56e62cdff07b57
positron <769230030@qq.com>
2026-06-23 17:41:50 +0800

    add keybinding for ToggleHLSearch

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,10 +15,44 @@ local opts = mapopts(nil)
 -- 显示时间喵
 map('n', '<F2>', ":echo '现在时间是' . strftime('%c') . ' 喵~'<CR>", opts)
 
+-- 调整窗口大小
+map('n', '<C-Up>', ':resize -2<CR>', opts)
+map('n', '<C-Down>', ':resize +2<CR>', opts)
+map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-- v模式连续调整缩进
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
+
+-- 更方便的注释
+-- 终端会把/识别为_，为什么呢()
+map('v', '<leader>/', ':s/^/', mapopts('manual comment'))
+
+-- ToggleHLSearch
+map('n', '<leader>hl', '<cmd>ToggleHLSearch<CR>', mapopts('Toggle hlsearch'))
+
+-- Lazygit
+map('n', '<leader>lg', '<cmd>LazyGit<CR>')
+
+-- 撤回，保存，HJKL
+map({'n', 'i', 'v'}, '<C-Z>', '<Esc>u')
+map({'n', 'i', 'v'}, '<C-S>', '<Esc>:w<CR>')
+map({'n', 'v'}, 'H', '^')
+map({'n', 'v'}, 'L', '$')
+-- map('n', 'J', 'j')
+map('i', 'jk', '<ESC>')
+
+-- NvimTree
+map({'n', 'i'}, '<A-f>', '<Esc>:NvimTreeFocus<CR>', opts)
+map({'n', 'i'}, '<A-e>', '<Esc>:NvimTreeToggle<CR>', opts)
+map('n', '<leader>q', ':qa<CR>', opts)
+
 -- lsp迁移
 map('n', 'gD', vim.lsp.buf.declaration, mapopts("LSP: Declaration"))
 map('n', 'gd', vim.lsp.buf.definition, mapopts("LSP: Definition"))
-map('n', 'gr', vim.lsp.buf.references, mapopts("LSP: References"))
+map('n', 'grr', vim.lsp.buf.references, mapopts("LSP: References"))
+map('n', 'grn', vim.lsp.buf.rename, mapopts("LSP: Rename"))
 map('n', 'gk', vim.lsp.buf.hover, mapopts("LSP: Activate Hover"))
 map('n', 'gI', vim.lsp.buf.implementation, mapopts("LSP: Implementation"))
 map('n', '<c-k>', vim.lsp.buf.signature_help, mapopts("LSP: Signature Help"))
@@ -67,34 +101,4 @@ map('n', '<leader>mb', ':make build<CR>', opts)
 -- map('n', '<leader>jb', ':MyJustArg build<CR>', opts)
 -- map('n', '<leader>jt', ':MyJustArg test<CR>', opts)
 -- map('n', '<leader>jv', ':MyJustArg run ', opts)
-
--- 调整窗口大小
-map('n', '<C-Up>', ':resize -2<CR>', opts)
-map('n', '<C-Down>', ':resize +2<CR>', opts)
-map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
-
--- v模式连续调整缩进
-map('v', '<', '<gv', opts)
-map('v', '>', '>gv', opts)
-
--- 更方便的注释
--- 终端会把/识别为_，为什么呢()
-map('v', '<leader>/', ':s/^/', mapopts('manual comment'))
-
--- 撤回，保存，HJKL
-map({'n', 'i', 'v'}, '<C-Z>', '<Esc>u')
-map({'n', 'i', 'v'}, '<C-S>', '<Esc>:w<CR>')
-map({'n', 'v'}, 'H', '^')
-map({'n', 'v'}, 'L', '$')
--- map('n', 'J', 'j')
-map('i', 'jk', '<ESC>')
-
--- NvimTree
-map({'n', 'i'}, '<A-f>', '<Esc>:NvimTreeFocus<CR>', opts)
-map({'n', 'i'}, '<A-e>', '<Esc>:NvimTreeToggle<CR>', opts)
-map('n', '<leader>q', ':qa<CR>', opts)
-
--- Lazygit
-map('n', '<leader>lg', '<cmd>LazyGit<CR>')
 
